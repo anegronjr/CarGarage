b750abae38a7de343a418f36c2bc6b288be71d85
Jen E <jen@wecancodeit.org>
2019-01-31 12:52:35 -0500

    Initial commit car garage converted to C#

---
+++ CarGarage/CarGarage.Tests/GarageTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xunit;
+
+namespace CarGarage.Tests
+{
+    public class GarageTests
+    {
+        [Fact]
+        public void shouldAddCarToGarage()
+        {
+            // Tests AddCar() method
+            // Act
+            // Arrange
+            // Assert
+        }
+
+        [Fact]
+        public void shouldRemoveCarFromGarage()
+        {
+            // Tests RemoveCar() method
+            // Act
+            // Arrange
+            // Assert
+        }
+
+        [Fact]
+        public void shouldFuelAllCars()
+        {
+            // Tests FuelAllCars() method
+            // Act
+            // Arrange
+            // Assert
+        }
+
+        [Fact]
+        public void shouldTestDriveOneCar()
+        {
+            // Tests DriveCar() method which lets you select a single car
+            // Program class should then let you choose what you want to do with that car
+            // Act
+            // Arrange
+            // Assert
+        }
+
+        [Fact]
+        public void youShouldNameThisTest()
+        {
+            // Should be able to list stats of all cars
+            // Create necessary method(s)
+            // Garage class should provide cars
+            // Program class should list all stats
+        }
+    }
+}
